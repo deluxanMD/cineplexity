f3a3a7ddb23c51f12965c26bd22a78226a9999d3
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2023-06-24 23:29:34 +0530

    app test suite

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,9 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('<App />', () => {
-  render(<App />);
-  const divElement = screen.getByTestId('App.Container');
-  expect(divElement).toBeInTheDocument();
+  it('should render the component', () => {
+    render(<App />);
+    const divElement = screen.getByTestId('App.Container');
+    expect(divElement).toBeInTheDocument();
+  });
 });
