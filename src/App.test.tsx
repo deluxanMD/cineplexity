8d0675ca1682eaa0e6e0f66301f56d18a40e73fb
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2023-06-24 23:26:16 +0530

    mui added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+describe('<App />', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const divElement = screen.getByTestId('App.Container');
+  expect(divElement).toBeInTheDocument();
 });
