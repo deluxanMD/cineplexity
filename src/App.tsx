4b68ca0caae671062ee87b773e20edd722833df0
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2023-06-26 22:08:22 +0530

    just for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { Button } from '@mui/material';
-
 const App = () => {
   return (
-    <div data-testid="App.Container">
-      <h5>App Component</h5>
-      <Button variant="contained">Test Me</Button>
+    <div data-testid="App.Containe">
+      <p>Route will be implemented</p>
     </div>
   );
 };
